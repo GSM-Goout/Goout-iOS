938674ef3a2bac31000eb48bfe257942e6e1b05a
jo030225 <whwngur123@naver.com>
2021-02-12 21:37:45 +0900

    [Add] 로그인 UI 개발 완료

---
+++ Goout/Goout/Controller/SignInViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SignInViewController.swift
+//  Goout
+//
+//  Created by 조주혁 on 2021/02/10.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    
+    @IBOutlet weak var uiview: UIView! {
+        didSet {
+            uiview.layer.cornerRadius = 50
+            uiview.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+            uiview.layer.shadowOpacity = 1
+            uiview.layer.shadowRadius = 10
+            uiview.layer.shadowOffset = CGSize(width: 0, height: 4)
+        }
+    }
+    @IBOutlet weak var signInBtn: UIButton! {
+        didSet {
+            signInBtn.layer.cornerRadius = 8
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
